eac73d7b7c21e2ac59ea12ceb6d2dbe1fb16dacd
supercmmetry <vishaals2000@gmail.com>
2020-03-26 17:02:52 +0530

    git: resolve merge conflict

--- pkg/entities/player.go
+++ pkg/entities/player.go
@@ -5,7 +5,7 @@ type Player struct {
 	Email    string `json:"email" gorm:"primary_key"`
 	Name     string `json:"name" gorm:"not null"`
 	Password string `json:"password" gorm:"not null"`
-	MaxScore    uint64 `json:"score"`
+	MaxScore uint64 `json:"score"`
 }
 
 type AbstractArtifact struct {
